3c62e6931235c0019845dc6595fef303e11ec2e9
Jackson Moody <jackson@jacksonmoody.com>
2025-09-22 21:21:04 -0400

    Update signin form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -39,7 +39,13 @@ export function LoginForm({
               </div>
               <div className="grid gap-3">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" name="password" type="password" required />
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  required
+                />
               </div>
               <div className="flex flex-col gap-3">
                 <Button type="submit" className="w-full">
